2df6d72feec7067414c8fdc8042c2682b273e5dc
TrevorNolan <146225324+TrevorNolan@users.noreply.github.com>
2024-02-08 18:46:05 -0500

    restaurant v2

--- Restaurant.py
+++ Restaurant.py
@@ -4,50 +4,73 @@
 
 #restaurant
 
+while True:
+    vegetarian = input("Is anyone in your party vegetarian?(yes/no): ").lower()
+    vegan = input("Is anyone in your party vegan?(yes/no): ").lower()
+    gluten_free = input("Is anyone in your party Gluten Free?(yes/no): ").lower()
+    print()
 
-vegitarian = input("Is anyone in your party vegitarian?(yes/no): ").lower()
-vegan = input("Is anyone in your party vegan?(yes/no): ").lower()
-gluten_free = input("is anyone in your party Gluten Free?(yes/no): ").lower()
-print()
-if vegitarian == "yes" and vegan == "yes" and gluten_free == "yes":
-    print("here are your restaurant choices." +
-          "\n"
-          "\nCorner cafe" +
-          "\nThe Chef's Kitchen")
-elif vegitarian == "yes" and vegan == "yes" and gluten_free == "no":
-    print("here are your restaurant choices." +
-          "\n"
-          "\nCorner cafe" +
-          "\nThe Chef's Kitchen")
-elif vegitarian == "yes" and vegan == "no" and gluten_free == "no":
-    print("here are your restaurant choices." +
-          "\n"
-          "\nCorner cafe" +
-          "\nThe Chef's Kitchen" +
-          "\nMain Street Pizza Company" +
-          "\nMamas Fine Italian")
-elif vegitarian == "no" and vegan == "no" and gluten_free == "no":
-    print("here are your restaurant choices." +
-          "\n"
-          "\nCorner cafe" +
-          "\nThe Chef's Kitchen" +
-          "\nMain Street Pizza Company" +
-          "\nMamas Fine Italian" +
-          "\nJoes Gourmet Burgers")
-elif vegitarian == "no" and vegan == "no" and gluten_free == "yes":
-    print("here are your restaurant choices." +
-          "\n"
-          "\nCorner cafe" +
-          "\nThe Chef's Kitchen" +
-          "\nMain Street Pizza Company")
+    if vegetarian == "yes" and vegan == "yes" and gluten_free == "yes":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen")
 
-elif vegitarian == "no" and vegan == "yes" and gluten_free == "yes":
-    print("here are your restaurant choices." +
-          "\n"
-          "\nCorner cafe" +
-          "\nThe Chef's Kitchen")
-elif vegitarian == "no" and vegan == "yes" and gluten_free == "no":
-    print("here are your restaurant choices." +
-          "\n"
-          "\nCorner cafe" +
-          "\nThe Chef's Kitchen")
+    elif vegetarian == "yes" and vegan == "yes" and gluten_free == "no":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen")
+
+    elif vegetarian == "yes" and vegan == "no" and gluten_free == "no":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen" +
+              "\nMain Street Pizza Company" +
+              "\nMama's Fine Italian")
+
+    elif vegetarian == "no" and vegan == "no" and gluten_free == "no":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen" +
+              "\nMain Street Pizza Company" +
+              "\nMama's Fine Italian" +
+              "\nJoe's Gourmet Burgers")
+
+    elif vegetarian == "no" and vegan == "no" and gluten_free == "yes":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen" +
+              "\nMain Street Pizza Company")
+
+    elif vegetarian == "no" and vegan == "yes" and gluten_free == "yes":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen")
+
+    elif vegetarian == "no" and vegan == "yes" and gluten_free == "no":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen")
+
+    elif vegetarian == "yes" and vegan == "no" and gluten_free == "yes":
+        print("Here are your restaurant choices:" +
+              "\n"
+              "\nMain Street Pizza Company" +
+              "\nCorner Cafe" +
+              "\nThe Chef's Kitchen")
+
+    else:
+        print("Invalid input! Please enter 'yes' or 'no'.")
+
+    print()
+    choice = input("Would you like to search again? (yes/no): ").lower()
+    print()
+    if choice != "yes":
+        break
+    
